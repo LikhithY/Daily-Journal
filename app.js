88c29e70448108482f3490ab74009285f2013210
Likhith <likhithy27@gmail.com>
2022-09-03 02:05:20 +0530

    Updated ports

--- app.js
+++ app.js
@@ -318,10 +318,6 @@ app.post("/delete", function (req, res) {
   }
 });
 
-let port = process.env.PORT;
-if(port == null) || port == ""){
-  port = 3000;
-}
-app.listen(port, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("server has started successfully ");
 });
